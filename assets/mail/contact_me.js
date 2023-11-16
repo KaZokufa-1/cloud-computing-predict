c7abf1840d76288feaca5268f9e9d72b8ff7e363
KaZokufa-1 <141829053+KaZokufa-1@users.noreply.github.com>
2023-11-16 14:38:59 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://mdwte1c21c.execute-api.eu-west-2.amazonaws.com/DE-ANDILE}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
